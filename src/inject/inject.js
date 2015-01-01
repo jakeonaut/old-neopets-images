ace48ba5e2c295410b3cedd3d1bad4ef3512a1a4
Jake Trower <jakeonaut@gmail.com>
2015-01-01 01:28:27 -0500

    got simple image id replacement (pet sidebar) working again

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,30 +3,34 @@ function loadScript(scriptName, callback){
 	s.src = chrome.extension.getURL('src/inject/'+scriptName+'.js');
 	s.onload = function(){
 		this.parentNode.removeChild(this);
-		callback();
+		if (callback)
+			callback();
 	};
 	(document.head||document.documentElement).appendChild(s);
 }
 
 function loadInit(callback){
+	//console.log("0");
 	loadScript('neopetsInit', callback);
 }
 
 function loadImageIdCollection(callback){
+	//console.log("1");
 	loadScript('neopetsCollection', callback);
 }
 
 function loadNewToOldImage(callback){
-	loadScript('neopetsOldToNew', callback);
+	//console.log("2");
+	loadScript('neopetsNewToOld', callback);
 }
 
 function loadMain(callback){
+	//console.log("3");
 	loadScript('neopetsMain', callback);
 }
 
-
-loadInit(
-	loadImageIdCollection(
-		loadNewToOldImage(
-			loadMain(function(){
+loadMain(
+	loadNewToOldImage(
+		loadImageIdCollection(
+			loadInit(function(){
 }))));
---
+++ src/inject/neopetsMain.js
@@ -0,0 +1,7 @@
+oldNeopetsChrome.change_new_images_to_old = true;
+oldNeopetsChrome.disable_pet_customization = true;
+
+console.log("I'M HERE BABY!!!");
+if (oldNeopetsChrome.change_new_images_to_old){
+	oldNeopetsChrome.ChangeAllImages();
+}
