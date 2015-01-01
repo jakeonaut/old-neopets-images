cc96a5eb4640a54c7a67acb0a85881a92857bbbd
Jake Trower <jakeonaut@gmail.com>
2015-01-01 11:21:14 -0500

    removed cached files?

--- src/inject/neopetsMain.js
+++
@@ -1,7 +0,0 @@
-oldNeopetsChrome.change_new_images_to_old = true;
-oldNeopetsChrome.disable_pet_customization = true;
-
-console.log("I'M HERE BABY!!!");
-if (oldNeopetsChrome.change_new_images_to_old){
-	oldNeopetsChrome.ChangeAllImages();
-}
